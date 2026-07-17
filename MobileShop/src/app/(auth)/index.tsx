a509c38dc02495fca8c75780b16a8f33ee6088df
Pasan Diksura <pasan.20210415@iit.ac.lk>
2026-07-17 09:04:02 +0530

    Cleaned the project

---
+++ MobileShop/src/app/(auth)/index.tsx
@@ -0,0 +1,18 @@
+import { StyleSheet, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function Index() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>This is Auth Screen</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
