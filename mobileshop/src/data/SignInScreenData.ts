0a3808556a6de8c281aed859d4141dba61ba8c06
Pasan Diksura <pasan.20210415@iit.ac.lk>
2026-07-21 13:29:11 +0530

    Create SignInScreenData.ts

---
+++ mobileshop/src/data/SignInScreenData.ts
@@ -0,0 +1,9 @@
+export const SignInScreenData = {
+    appName: 'Mobile Shop',
+    subTitle: 'Manage Mobile Distribution',
+    welcome: "Welcome Back",
+    loginDescription: "Sign in to continue and start using your personalised app experience.",
+    submitBtn: "Sign In",
+    signingIn: "Signing In...",
+    TnC: "By continuing, you agree to our Terms and Privacy Policy",
+}
