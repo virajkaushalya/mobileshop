dfa4b36d69c1b2125469f57414eae7c64f846335
Pasan Diksura <pasan.20210415@iit.ac.lk>
2026-07-21 13:28:27 +0530

    Added new colour theme

--- mobileshop/tailwind.config.js
+++ mobileshop/tailwind.config.js
@@ -1,10 +1,70 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // NOTE: Update this to include the paths to all files that contain Nativewind classes.
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  presets: [require("nativewind/preset")],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    // NOTE: Update this to include the paths to all files that contain Nativewind classes.
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    presets: [require("nativewind/preset")],
+    darkMode: "media",
+    theme: {
+        extend: {
+            colors: {
+
+                background: "hsl(var(--background) / <alpha-value>)",
+                foreground: "hsl(var(--foreground) / <alpha-value>)",
+
+                card: {
+                    DEFAULT: "hsl(var(--card) / <alpha-value>)",
+                    foreground: "hsl(var(--card-foreground) / <alpha-value>)",
+                },
+
+                primary: {
+                    DEFAULT: "hsl(var(--primary) / <alpha-value>)",
+                    foreground: "hsl(var(--primary-foreground) / <alpha-value>)",
+                },
+
+                secondary: {
+                    DEFAULT: "hsl(var(--secondary) / <alpha-value>)",
+                    foreground: "hsl(var(--secondary-foreground) / <alpha-value>)",
+                },
+
+                muted: {
+                    DEFAULT: "hsl(var(--muted) / <alpha-value>)",
+                    foreground: "hsl(var(--muted-foreground) <alpha-value>)",
+                },
+
+                accent: "hsl(var(--accent) / <alpha-value>)",
+
+                destructive: {
+                    DEFAULT: "hsl(var(--destructive) / <alpha-value>)",
+                    foreground: "hsl(var(--destructive-foreground) / <alpha-value>)",
+                },
+
+                border: "hsl(var(--border) / <alpha-value>)",
+
+                input: "hsl(var(--input) / <alpha-value>)",
+
+                ring: "hsl(var(--ring) / <alpha-value>)",
+
+                success: "hsl(var(--success) / <alpha-value>)",
+
+                priority: {
+                    low: {
+                        DEFAULT: "hsl(var(--priority-low) / <alpha-value>)",
+                        foreground: "hsl(var(--priority-low-foreground) / <alpha-value>)",
+                    },
+
+                    medium: {
+                        DEFAULT: "hsl(var(--priority-medium) / <alpha-value>)",
+                        foreground: "hsl(var(--priority-medium-foreground) / <alpha-value>)",
+                    },
+
+                    high: {
+                        DEFAULT: "hsl(var(--priority-high) / <alpha-value>)",
+                        foreground: "hsl(var(--priority-high-foreground) / <alpha-value>)",
+                    },
+                }
+
+            }
+        },
+    },
+    plugins: [],
 }
