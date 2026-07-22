2df3c960c0ec1c98fd2bacf1e8e53ce21a137e48
Pasan Diksura <pasan.20210415@iit.ac.lk>
2026-07-22 16:21:27 +0530

    Create HomeScreenData.js

---
+++ mobileshop/src/data/HomeScreenData.js
@@ -0,0 +1,10 @@
+export const HomeScreenData = {
+    greeting: "Welcome",
+    welcomeText: "Welcome back to delivery-hub. Let's make some progress",
+    progressText: "Current Progress",
+    progressStatus: {
+        notStarted: "No Progress",
+        inProgress: "In Progress...",
+        completed: "Task Completed",
+    }
+}
