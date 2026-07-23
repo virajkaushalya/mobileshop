332366b243d699a5a1a98ec9638bc99aa6889b07
Pasan Diksura <pasan.20210415@iit.ac.lk>
2026-07-23 15:36:03 +0530

    Create VisitScreenData.js

---
+++ mobileshop/src/data/VisitScreenData.js
@@ -0,0 +1,24 @@
+export const VisitScreenData = {
+    request: {
+        title: "Requests From Shop",
+        description: "Any requests regarding restocking or stock related data records here",
+        placeholder: "Enter requests here"
+    },
+    remark: {
+        title: "Remarks",
+        description: "Any remarks related data records here",
+        placeholder: "Enter remarks here"
+    },
+    receiving: {
+        title: "Payment Details",
+        description: "All payments records from this shop add here",
+        cash: "Cash Receiving",
+        cashDescription: "Enter the total of cash Receiving",
+        cheque: "Cheque Receiving",
+        cashPlaceholder: "Add amount",
+        chequeDescription: "Enter the total of cheque Receiving details",
+        chequeNumberPlaceholder: "Add cheque Number",
+        chequeAmountPlaceholder: "Add amount",
+        images: "Insert images of received cheques"
+    }
+}
