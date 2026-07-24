368618fefba43326216d5c3b033a77daf31d3ec0
Pasan Diksura <pasan.20210415@iit.ac.lk>
2026-07-24 15:58:40 +0530

    Add Daily Report screen components and data
    Add PaymentPill, ShopPaymentCard, and SummaryTile components for the Daily Report screen. These components display shop payment collections including cash and cheque details. Include ReportScreenData constants and temporary dummy payment data for development.

---
+++ mobileshop/src/temp/dummyData.js
@@ -0,0 +1,28 @@
+export const visitPayments = [
+    {
+        id: "1",
+        shopId: "12dsa31s",
+        shopName: "ABC Mobile",
+        cash: 45000,
+        cheques: [
+            {number: "001245", amount: 70000},
+            {number: "001246", amount: 50000},
+        ],
+    },
+    {
+        id: "2",
+        shopId: "87kas12",
+        shopName: "GenXT Mobiles",
+        cash: 28000,
+        cheques: [
+            {number: "774210", amount: 42000},
+        ],
+    },
+    {
+        id: "3",
+        shopId: "55msa82",
+        shopName: "City Cell House",
+        cash: 16000,
+        cheques: [],
+    },
+];
