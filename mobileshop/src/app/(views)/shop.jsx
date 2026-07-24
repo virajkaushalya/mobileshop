926c676996ae8c7bda656b56cfb052e38bf04fbd
Pasan Diksura <pasan.20210415@iit.ac.lk>
2026-07-24 10:20:22 +0530

    Implement tabbed navigation in Shop page
    Replace the Shop placeholder with a full tabbed interface featuring Visit and Delivery tabs. Add tab state management, dark mode support, and integrate the ScreenBackground and TabButton components. Set the screen title to 'GenEX Mobile'.

--- mobileshop/src/app/(views)/shop.jsx
+++ mobileshop/src/app/(views)/shop.jsx
@@ -1,11 +1,51 @@
-import {Text, View} from "react-native";
-import {SafeAreaView} from "react-native-safe-area-context";
+import {View, useColorScheme} from "react-native";
+import Visit from "./tabs/visit";
+import ScreenBackground from "../../components/ScreenBackground";
+import {useState} from "react";
+import Delivery from "./tabs/delivery";
+import {Stack} from "expo-router";
+import {TabButton} from "../../components/TabButton";
 
 const Shop = () => {
+
+    const [currentTab, setCurrentTab] = useState("visit");
+
+    const colorScheme = useColorScheme();
+    const isDark = colorScheme === "dark";
+
     return (
-        <SafeAreaView>
-            <Text className={'text-foreground'}>This is Shop page</Text>
-        </SafeAreaView>
+
+        <>
+            <Stack.Screen options={{title: "GenEX Mobile"}}/>
+
+            <ScreenBackground/>
+
+
+            <View className={'mt-4 mx-16 flex-row bg-white/60 dark:bg-secondary/60 rounded-2xl'}>
+
+                <TabButton
+                    title={'Visit'}
+                    active={currentTab === "visit"}
+                    isDark={isDark}
+                    onPress={() => setCurrentTab("visit")}
+                />
+
+                <TabButton
+                    title={'Delivery'}
+                    active={currentTab === "delivery"}
+                    isDark={isDark}
+                    onPress={() => setCurrentTab("delivery")}
+                />
+
+            </View>
+
+            <View className={'flex-1'}>
+                {currentTab === "visit" && <Visit/>}
+                {currentTab === "delivery" && <Delivery/>}
+            </View>
+
+
+        </>
     );
 }
 
