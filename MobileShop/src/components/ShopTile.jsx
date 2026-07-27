f1b70064de86a82b04a5f635de847d21c0a4b471
Pasan Diksura <pasan.20210415@iit.ac.lk>
2026-07-27 13:26:28 +0530

    Update ShopTile.jsx

--- MobileShop/src/components/ShopTile.jsx
+++ MobileShop/src/components/ShopTile.jsx
@@ -1,6 +1,7 @@
 import {Pressable, Text, useColorScheme, View} from "react-native";
 import FontAwesome from "@react-native-vector-icons/fontawesome";
-import {Redirect, router} from "expo-router";
+import {router} from "expo-router";
+import {formatMobileNumber} from "../ulitily/inputFormatter";
 
 const ShopTile = ({shopName, address, mobileNumber}) => {
 
@@ -11,22 +12,6 @@ const ShopTile = ({shopName, address, mobileNumber}) => {
     const processedAddress = !address
         ? "Unknown Address"
         : address.split(",").map(s => s.trim()).join(", \n");
-
-    /**
-     * First process to get only digit, in case if number contains '-' or spaces. e.g. 071-2345-123 (these numbers will still format)
-     * then it process to format the given number
-     **/
-    const processedNumber = () => {
-        if (!mobileNumber) return "No mobile number";
-
-        const number = String(mobileNumber).replace(/\D/g, "");
-
-        if (number.length === 10 && number.startsWith("07")) {
-            return `(${number.slice(0, 3)}) ${number.slice(3, 6)} ${number.slice(6)}`;
-        }
-
-        return mobileNumber;
-    };
 
 
     return (
@@ -43,18 +28,20 @@ const ShopTile = ({shopName, address, mobileNumber}) => {
             <View className={'flex-1 flex-col'}>
                 <Text className={'text-foreground text-2xl uppercase mb-2'}>{shopName}</Text>
                 <View className={'flex-row justify-between'}>
-                    <Text
-                        className={'text-foreground/60 text-sm flex-1'}
-                        numberOfLines={4}
-                        ellipsizeMode={"tail"}
-                    >
-                        {processedAddress}
-                    </Text>
+                    <View className="flex-1 h-16 justify-end">
+                        <Text
+                            className="text-foreground/60 text-sm"
+                            numberOfLines={3}
+                            ellipsizeMode="tail"
+                        >
+                            {processedAddress}
+                        </Text>
+                    </View>
 
                     {mobileNumber && (
                         <View className={'justify-end align-bottom h-max pr-8 ml-4'}>
                             <Text className={'text-foreground/60 w-max text-right text-sm font-semibold'}>:Mobile</Text>
-                            <Text className={'text-foreground/60 w-max text-right text-sm'}>{processedNumber()}</Text>
+                            <Text className={'text-foreground/60 w-max text-right text-sm'}>{formatMobileNumber(mobileNumber)}</Text>
                         </View>
                     )}
 
