ce60a5aa1eae51c88d0764d7f15646f2fcbd7eb4
Pasan Diksura <pasan.20210415@iit.ac.lk>
2026-07-28 14:23:20 +0530

    Update SingleLineTextField.jsx

--- mobileshop/src/components/SingleLineTextField.jsx
+++ mobileshop/src/components/SingleLineTextField.jsx
@@ -2,6 +2,7 @@ import "../../global.css"
 
 import {Text, TextInput, useColorScheme, View} from "react-native";
 import TitleDescriptionTile from "./TitleDescriptionTile";
+import TextFieldErrorMessage from "./TextFieldErrorMessage";
 
 const SingleLineTextField = ({
                                  isHaveTitle = true,
@@ -14,8 +15,11 @@ const SingleLineTextField = ({
                                  onChangeText,
                                  readOnly = false,
                                  flexIndex = 1,
+                                 errorMessage = "",
+                                 maxLength = null,
                              }) => {
 
+    const isError = errorMessage;
     const colourScheme = useColorScheme();
     const placeholderColour = colourScheme === "dark" ? "hsl(219 9% 44%)" : "hsl(219 9% 44%)";
 
@@ -24,7 +28,8 @@ const SingleLineTextField = ({
 
             {isHaveTitle && (<TitleDescriptionTile title={title} description={description}/>)}
 
-            <View className={`mb-5 px-2 flex-row items-center rounded-2xl border border-border bg-card focus:border-ring`}>
+            <View
+                className={`px-2 flex-row items-center rounded-2xl border border-border bg-card ${(isError ? 'border-red-600 focus:border-red-600' : 'mb-5 focus:border-ring')}`}>
                 <TextInput
                     className={`flex-1 text-foreground`}
                     value={value}
@@ -37,8 +42,11 @@ const SingleLineTextField = ({
                     multiline={false}
                     onChangeText={onChangeText}
                     readOnly={readOnly}
+                    maxLength={maxLength}
                 />
             </View>
+
+            {isError && <TextFieldErrorMessage errorMessage={errorMessage} marginBottom={'mb-5'}/>}
 
         </View>
     )
