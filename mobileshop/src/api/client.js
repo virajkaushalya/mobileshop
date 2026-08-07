ea6b6bd4679c0a55de9d4566b78f04648fd36cdf
Pasan Diksura <pasan.20210415@iit.ac.lk>
2026-08-07 15:10:55 +0530

    Update client.js

--- mobileshop/src/api/client.js
+++ mobileshop/src/api/client.js
@@ -1,12 +1,28 @@
 import axios from "axios";
+import {getToken} from "../services/authStoreService";
 
 const apiClient = axios.create({
     baseURL: "http://10.0.2.2/MobileShopAPI/api/api.php",
     timeout: 10000,
-    headers:{
-        "Content-Type":"application/json"
+    headers: {
+        "Content-Type": "application/json"
     }
 });
 
+apiClient.interceptors.request.use(
+    (config) => {
+
+        const token = getToken();
+
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
 
 export default apiClient;
