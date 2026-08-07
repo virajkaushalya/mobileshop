ed3f4c0af988da864db100a45a326b6ae1a1e6c9
Pasan Diksura <95087303+Diksura@users.noreply.github.com>
2026-08-07 11:40:34 +0530

    Delete MobileShop directory

--- MobileShop/src/app/(views)/_layout.jsx
+++
@@ -1,7 +0,0 @@
-import { Stack } from "expo-router";
-
-export default function ViewsLayout() {
-    return (
-        <Stack screenOptions={{ headerShown: true }} />
-    );
-}
--- MobileShop/src/services/authService.js
+++
@@ -1,5 +0,0 @@
-export function authService({email, password}) {
-
-    // TODO: process auth connect with api and store JWT
-    return true;
-}
