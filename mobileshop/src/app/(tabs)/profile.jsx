e68674d379f337f5539c4dfa6fc724e24d7401a0
Pasan Diksura <pasan.20210415@iit.ac.lk>
2026-08-10 13:12:26 +0530

    Update profile.jsx

--- mobileshop/src/app/(tabs)/profile.jsx
+++ mobileshop/src/app/(tabs)/profile.jsx
@@ -13,7 +13,7 @@ const Profile = () => {
 
                     <Text className={'text-foreground/80 text-3xl font-semibold'}>Profile Settings</Text>
 
-                    // TODO
+                    {/*TODO*/}
 
                 </SafeAreaView>
             </View>
