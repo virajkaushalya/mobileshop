a5a94ceba5861a581ee085a45100e3039e22e2b1
Pasan Diksura <pasan.20210415@iit.ac.lk>
2026-08-12 10:14:39 +0530

    Update client.js

--- mobileshop/src/api/client.js
+++ mobileshop/src/api/client.js
@@ -1,8 +1,13 @@
 import axios from "axios";
 import {getToken} from "../services/authStoreService";
 
+// const  BASE_URL = "http://10.0.2.2";  // Android Emulator
+// const  BASE_URL = "http://localhost"; // iOS Simulator
+const BASE_URL = "http://192.168.8.106"; // Redmi Mobile
+
+
 const client = axios.create({
-    baseURL: "http://10.0.2.2/MobileShopAPI/api/api.php",
+    baseURL: `${BASE_URL}/MobileShopAPI/api/api.php`,
     timeout: 10000,
     headers: {
         "Content-Type": "application/json",
